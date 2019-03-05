5e4ebb4b12a9ddc770ce45e61581d6e37a4a2259
Philip <philipax@kth.se>
2019-03-05 22:42:21 +0100

    navbar change and popup

--- carboncal/src/TopBar/TopBar.js
+++ carboncal/src/TopBar/TopBar.js
@@ -12,31 +12,29 @@ class TopBar extends Component {
     render() { 
         return (
         <React.Fragment>
-            <div id="headerWrapper">
+            <nav id="navBar" className="navbar fixed-top navbar-dark bg-dark">
                 <div className="container h-100">
-                    <nav id="navBar" className="navbar navbar navbar-dark justify-content-between">
                     <Link to="/searchTravel">
                         <span className="navbar-brand">Home</span>
-                        </Link>
-                    
-                        <div className="btn-group">
-                            <button type="button" className="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Action
+                    </Link>
+                    <Link to="/UserTravels">
+                        <button className="btn btn-info justify-content-center">
+                            <i class="fas fa-th-list mr-2"></i>My list
+                        </button>
+                    </Link>
+                    <div className="btn-group">
+                        <button type="button" className="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-cog mr-2"></i>Settings
+                        </button>
+                        <div className="dropdown-menu">
+                            <span className="dropdown-item" href="#">My Account</span>
+                            <div className="dropdown-divider"></div>
+                            <button className="dropdown-item" id="logOutButton" href="#">Log Out
                             </button>
-                            <div className="dropdown-menu">
-                                <span className="dropdown-item" href="#">My Account</span>
-                                <Link to="/UserTravels">
-                                    <span className="dropdown-item" href="#">My List</span>
-                                </Link>
-                                <div className="dropdown-divider"></div>
-                                <button className="dropdown-item" id="logOutButton" href="#">Log Out</button>
-                            </div>
                         </div>
-
-
-                    </nav>
+                    </div>
                 </div>
-            </div>
+            </nav>
 
             <div className="container h-100" id="infoTextContainer">
                 <div className="d-flex justify-content-center h-100">
@@ -50,7 +48,7 @@ class TopBar extends Component {
                     </div>
                     <div className="col-sm-4 m-2">
                         <span><i id="infoSymbolThree" className="fas fa-check-circle m-2"></i></span>
-                        <span id="infoText">Get the result</span>
+                        <span id="infoText">Receive your result</span>
                     </div>
                 </div>
             </div>
