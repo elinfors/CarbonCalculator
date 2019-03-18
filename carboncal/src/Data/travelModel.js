84106368660204d365539a81b3cd402943078bd0
viktorle <35763150+viktorle@users.noreply.github.com>
2019-03-18 10:45:06 +0100

    Revert "Flygresor.se"
    This reverts commit b0d265385c7b6192b3f8245af0bfb97fb6bad2fc.

--- carboncal/src/Data/travelModel.js
+++ carboncal/src/Data/travelModel.js
@@ -63,11 +63,7 @@ class TravelModel extends ObservableModel {
 
     saveTravel(travel){
         let date = new Date();
-        let month, day = 0
-        date.getDate() < 10 ? day = "0" + date.getDate(): day = date.getDate();
-        date.getMonth() < 10 ? month = "0" + date.getMonth(): month = date.getMonth();
-        travel["date"] = day + " " + month + " " + date.getFullYear();
-
+        travel["date"] = date.getDate() + " " + date.getMonth() + " " + date.getFullYear();
         console.log(travel["date"])
         this.savedTravels.push(travel);
         this.notifyObservers();
@@ -77,7 +73,7 @@ class TravelModel extends ObservableModel {
     removeSavedTravel(travel){
           let list = this.savedTravels;
           for (var i in list){
-              if (travel.id === list[i].id && travel.travelType === list[i].travelType){
+              if (travel.id === list[i].id){
                   list.splice(i,1);
               }
           }
@@ -94,7 +90,7 @@ class TravelModel extends ObservableModel {
       ))
       travel["AllTravelTypeEmission"] = AllTravelTypeEmission;
       travel["maxEmission"] = Math.max.apply(null, AllTravelTypeEmission);
-      travel["minEmission"] = Math.min.apply(null, AllTravelTypeEmission);
+      travel["minEmission"] = Math.min.apply(null, AllTravelTypeEmission)
       this.compareTravels.push(travel);      
       this.notifyObservers();
       console.log(this.compareTravels);
@@ -103,7 +99,7 @@ class TravelModel extends ObservableModel {
     removeComparedTravel(travel){
       let allCompares = this.compareTravels;
       for (let i in allCompares){
-          if (travel.id === allCompares[i].id && travel.travelType === allCompares[i].travelType ){
+          if (travel.id === allCompares[i].id){
               allCompares.splice(i,1);
           }
       }
@@ -128,7 +124,7 @@ class TravelModel extends ObservableModel {
     removeResult(travel){
           let results = this.allResults;
           for (let i in results){
-              if (travel.id === results[i].id && travel.travelType === results[i].travelType){
+              if (travel.id === results[i].id){
                   results.splice(i,1);
               }
           }
